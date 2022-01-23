8acef3967aef4ae91254207cedbebd5b169177b3
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2022-01-23 19:09:41 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 long long banyak_pengecekan = 0;
 vector<vector<int> > vecX;
 vector<vector<int> > vecY;
+vector<vector<int> > Penunjuk_arah;
 vector<string>tampung_kata;
 vector<vector<char> >tampung_char;
 bool file_berhasil_dibuka = true;
@@ -26,7 +27,7 @@ void convertTXT(){
     // Membuka File Inputan
 
     ifstream ifs;
-    ifs.open("../test/puzzle2.txt");
+    ifs.open("../test/stars.txt");
 
     if (ifs.is_open()){
 
@@ -89,15 +90,17 @@ bool Search_8_Direction(string word, vector<vector<char> >tampung_char, int row,
 }
 
 // Fungsi untuk mengecek apakah indeks ke indeks_row = vecY dan dan indeks_col = vecX 
-bool Check_Indeks(vector<vector<int> > vecX, vector<vector<int> > vecY, int indeks_row, int indeks_col){
+// Jika benar akan mengembalikan sesuai dengan arahnya yaitu 1,2,3,..,8
+// Jika salah maka akan mengembalikan 0 
+int Check_Indeks(vector<vector<int> > vecX, vector<vector<int> > vecY, int indeks_row, int indeks_col){
     for (int k = 0; k < vecX.size(); k++){
         for (int L = 0; L < vecX[k].size(); L++){
             if ((indeks_row == vecY[k][L]) & (indeks_col == vecX[k][L])){
-                return true;
+                return Penunjuk_arah[k][L];
             }
         }
     }
-    return false;
+    return 0;
 }
 
 // Procedure untuk memasukkan setiap koordinat dari setiap karakter 
@@ -114,14 +117,42 @@ void Simpan_koordinat(vector<vector<char> >tampung_char, string word){
                     // Dengan cara memasukkan masing-masing absis dan ordinat tersebut ke dalam tempX dan tempY 
                     // kemudian masukkan ke dalam vecX dan VecY
                     if (Search_8_Direction(word, tampung_char, row, col, moveX[arah], moveY[arah])){
+                        vector<int> tempPenunjukArah;
                         vector<int> tempX;
                         vector<int> tempY;
                         for (int i = 0; i < word.length(); i++){
                             tempX.push_back(col + (i * moveX[arah]));
                             tempY.push_back(row + (i * moveY[arah]));
+
+                            // Menyimpan arah dari setiap titik
+                            if (moveX[arah] == -1 & moveY[arah] == -1){
+                                tempPenunjukArah.push_back(1);
+                            }
+                            else if (moveX[arah] == -1 & moveY[arah] == 1){
+                                tempPenunjukArah.push_back(2);
+                            }
+                            else if (moveX[arah] == 1 & moveY[arah] == -1){
+                                tempPenunjukArah.push_back(3);
+                            }
+                            else if (moveX[arah] == 1 & moveY[arah] == 1){
+                                tempPenunjukArah.push_back(4);
+                            }
+                            else if (moveX[arah] == 0 & moveY[arah] == -1){
+                                tempPenunjukArah.push_back(5);
+                            }
+                            else if (moveX[arah] == -1 & moveY[arah] == 0){
+                                tempPenunjukArah.push_back(6);
+                            }
+                            else if (moveX[arah] == 0 & moveY[arah] == 1){
+                                tempPenunjukArah.push_back(7);
+                            }
+                            else if (moveX[arah] == 1 & moveY[arah] == 0){
+                                tempPenunjukArah.push_back(8);
+                            }
                         }
                         vecX.push_back(tempX);
                         vecY.push_back(tempY);
+                        Penunjuk_arah.push_back(tempPenunjukArah);
                     } 
                 }
             }
@@ -134,12 +165,41 @@ void Simpan_koordinat(vector<vector<char> >tampung_char, string word){
 void PrintSolusi(){
     for (int i = 0; i < tampung_char.size(); i++){
         for (int j = 0; j < tampung_char[i].size(); j++){
-            if (Check_Indeks(vecX, vecY, i, j)){
-                // Memberikan warna Hijau pada setiap karakter word yang wordnya ditemukan di puzzle
+            // Mewarnai karakter dengan warna tertentu
+            // Sesuai dengan arahnya
+            if (Check_Indeks(vecX, vecY, i, j) == 1){
+                SetConsoleTextAttribute(h, 1);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 2){
                 SetConsoleTextAttribute(h, 2);
                 cout << tampung_char[i][j] << " ";
             }
-            else{
+            else if (Check_Indeks(vecX, vecY, i, j) == 3){
+                SetConsoleTextAttribute(h, 4);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 4){
+                SetConsoleTextAttribute(h, 5);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 5){
+                SetConsoleTextAttribute(h, 6);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 6){
+                SetConsoleTextAttribute(h, 9);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 7){
+                SetConsoleTextAttribute(h, 3);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 8){
+                SetConsoleTextAttribute(h, 13);
+                cout << tampung_char[i][j] << " ";
+            }
+            else if (Check_Indeks(vecX, vecY, i, j) == 0){
                 // Jika tidak warnai putih pada setiap karakter yang akan diprint
                 SetConsoleTextAttribute(h, 7);
                 cout << tampung_char[i][j] << " ";
